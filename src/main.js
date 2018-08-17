9f5b9c5ef4307f5971e3dc4f73ae69063df2986f
Thiago Ferrarezi <thiago.ferrarezi@sumicity.net.br>
2018-08-17 12:31:20 -0300

    plugins

--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Firebase from 'firebase'
+import store from './store'
+import VueCurrencyFilter from 'vue-currency-filter'
+import VueResource from 'vue-resource'
+import VueMoment from 'vue-moment'
+import moment from 'moment'
+import Buefy from 'buefy'
+import VeeValidate, { Validator } from 'vee-validate'
 
 Vue.config.productionTip = false
+
+Validator.localize('pt_BR', ptBR)
+moment.locale('pt-BR')
+
+Vue.use(Firebase)
+Vue.use(Buefy, {defaultIconPack: 'fas'})
+Vue.use(VueResource)
+Vue.use(VueMoment, {moment})
+Vue.use(VeeValidate, { locale: 'pt_BR', deleay: '5' })
+Vue.use(VueCurrencyFilter,
+  {
+    symbol: 'R$',
+    thousandsSeparator: '.',
+    fractionCount: 2,
+    fractionSeparator: ',',
+    symbolPosition: 'front',
+    symbolSpacing: true
+  })
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
