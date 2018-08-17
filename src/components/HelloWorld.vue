701abc8244f4aa3683ed2ea6128ae2e6dcad46cc
Thiago Ferrarezi <thiago.ferrarezi@sumicity.net.br>
2018-08-17 18:09:30 -0300

    firebase

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,12 +5,14 @@
     <div>teste</div>
 
     <b-field label="Name">
-      <b-input v-model="cliente.nome"></b-input>
+      <b-input v-model="nome"></b-input>
     </b-field>
 
     <b-field label="Name">
-      <b-input v-model="cliente.telefone"></b-input>
+      <b-input v-model="telefone"></b-input>
     </b-field>
+
+    <button @click="teste"/>
   </div>
 </template>
 
@@ -19,18 +21,20 @@ import firebase from 'firebase'
 
 export default {
   name: 'HelloWorld',
-  created () {
-    this.teste()
-  },
+  created () {},
   data () {
     return {
-      cliente: {nome: null, telefone: null}
+      nome: null,
+      telefone: null
     }
   },
   methods: {
     teste () {
       let clientes = firebase.database().ref('clientes')
-      clientes.push()
+      clientes.push({
+        nome: this.nome,
+        telefone: this.telefone
+      })
     }
   }
 }
